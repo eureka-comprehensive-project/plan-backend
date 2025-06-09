de8418336f83129cc9dd684a39232867d657e439
Jimin Byun <fraise3838@naver.com>
2025-06-09 20:32:33 +0900

    fix : enum 수정

--- src/main/java/com/comprehensive/eureka/plan/enums/BenefitType.java
+++ src/main/java/com/comprehensive/eureka/plan/enums/BenefitType.java
@@ -1,5 +1,20 @@
 package com.comprehensive.eureka.plan.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum BenefitType {
-    프리미엄, 미디어;
+    PREMIUM("프리미엄"),
+    MEDIA("미디어"),
+    BASIC("기본");
+
+    private final String label;
+
+    BenefitType(String label) {
+        this.label = label;
+    }
+
+    public BenefitType[] getAllBenefitType() {
+        return BenefitType.values();
+    }
 }
--- src/main/java/com/comprehensive/eureka/plan/enums/DataPeriod.java
+++ src/main/java/com/comprehensive/eureka/plan/enums/DataPeriod.java
@@ -1,5 +1,16 @@
 package com.comprehensive.eureka.plan.enums;
 
 public enum DataPeriod {
-    일, 월;
+    DAY("일"),
+    MONTH("월");
+
+    private final String label;
+
+    DataPeriod(String label) {
+        this.label = label;
+    }
+
+    public DataPeriod[] getAllDataPeriod() {
+        return DataPeriod.values();
+    }
 }
