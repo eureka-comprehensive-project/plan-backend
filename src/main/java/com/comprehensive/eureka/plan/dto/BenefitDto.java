cbcd452ef7fd9c26ba315f534d3e747c73dfd292
Jimin Byun <fraise3838@naver.com>
2025-06-16 13:58:37 +0900

    feat : 필터 페이지에서 혜택 나열 기능 구현

--- src/main/java/com/comprehensive/eureka/plan/dto/BenefitDto.java
+++ src/main/java/com/comprehensive/eureka/plan/dto/BenefitDto.java
@@ -1,11 +1,15 @@
 package com.comprehensive.eureka.plan.dto;
 
 import com.comprehensive.eureka.plan.entity.enums.BenefitType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BenefitDto {
 
     private Long benefitId;
