67406c6d4f59201bc550ef594e1c1b9047b320a9
Jimin Byun <fraise3838@naver.com>
2025-06-26 16:22:10 +0900

    fix : 테스트 코드를 위한 어노테이션 추가

--- src/main/java/com/comprehensive/eureka/plan/dto/request/GetPlanBenefitGroupIdRequestDto.java
+++ src/main/java/com/comprehensive/eureka/plan/dto/request/GetPlanBenefitGroupIdRequestDto.java
@@ -1,8 +1,10 @@
 package com.comprehensive.eureka.plan.dto.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class GetPlanBenefitGroupIdRequestDto {
     private Integer planId;
     private Long benefitGroupId;
--- src/main/java/com/comprehensive/eureka/plan/dto/request/PlanFilterRequestDto.java
+++ src/main/java/com/comprehensive/eureka/plan/dto/request/PlanFilterRequestDto.java
@@ -1,5 +1,6 @@
 package com.comprehensive.eureka.plan.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@Builder
 public class PlanFilterRequestDto {
 
     private List<Long> categoryIds;
